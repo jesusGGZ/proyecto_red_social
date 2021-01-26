1d72ce3610031ef0a379b7f54e8aac1106cc4744
jesusGGZ <jesusgarcia@morpheusdss.com>
2021-01-26 12:00:18 -0600

    Completando funciones sql

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ module.exports = {
     },
     mysql: {
         host: process.env.MYSQL_HOST || 'localhost',
-        user: process.env.MYSQL_USER || 'vyU7DCqzKX',
-        password: process.env.MYSQL_PASS || 'hQ8gtxIQ9o',
-        database: process.env.MYSQL_DB || 'vyU7DCqzKX',
+        user: process.env.MYSQL_USER || 'root',
+        password: process.env.MYSQL_PASS || '',
+        database: process.env.MYSQL_DB || 'redSocial',
     },
 }
